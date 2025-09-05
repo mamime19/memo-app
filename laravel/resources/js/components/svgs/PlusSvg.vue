d7df255a8e39a54258a4bacc8bbcb387d7425029
mamime19 <tatsu25sun@gmail.com>
2025-09-05 12:15:36 +0900

    create textareaform

--- laravel/resources/js/components/svgs/PlusSvg.vue
+++ laravel/resources/js/components/svgs/PlusSvg.vue
@@ -5,6 +5,10 @@ export default {
             type: Number,
             default: 20,
         },
+        color: {
+            type:String,
+            default: "currentColor"
+        },
     }
 }
 </script>
@@ -17,7 +21,7 @@ export default {
     v-bind:height="size"
     viewBox="0 0 24 24"
     fill="none"
-    stroke="currentColor"
+    v-bind:stroke="color"
     stroke-width="2"
     stroke-linecap="round"
     stroke-linejoin="round"
